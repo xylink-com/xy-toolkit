8b63929db446e26ef68bb84e362353f55fdb03e5
wuqingshan <wuqingshan@xylink.com>
2021-01-28 16:47:48 +0800

    feat: get date info

---
+++ _test_/moment.test.ts
@@ -0,0 +1,89 @@
+const moment = require("../src/time/moment").default;
+// moment().locale("en_US");
+// moment().locale("zh_TW");
+describe("momentJS", () => {
+  it("current time", () => {
+    // expect(moment().year()).toEqual(new Date().getFullYear());
+    // expect(moment().shortYear()).toEqual(21);
+    // expect(moment().month()).toEqual(new Date().getMonth() + 1);
+    // expect(moment().date()).toEqual(new Date().getDate());
+    // expect(moment().day()).toEqual(new Date().getDay());
+    // expect(moment().hours()).toEqual(new Date().getHours());
+    // expect(moment().minutes()).toEqual(new Date().getMinutes());
+    // expect(moment().seconds()).toEqual(new Date().getSeconds());
+    // console.log(moment().localeHours());
+    // console.log(moment().toArray());
+    // console.log(moment().toObject());
+    // console.log(moment().day());
+    // console.log(moment().shortYear());
+    // console.log(moment().localeDay());
+    // console.log(moment().localeMonth());
+    // console.log(moment().padMonth());
+    // console.log(moment().quarter());
+    // console.log(moment().dayOfYear('2021-2-2'));
+    // console.log(moment().dayOfMonth('2021-2-2'));
+    // console.log(moment().toString());
+    // console.log(moment().toLocaleString());
+    // console.log(moment().toDateString());
+    // console.log(moment().toLocaleDateString());
+    // console.log(moment().toTimeString());
+    // console.log(moment().toLocaleTimeString());
+  });
+});
+
+// describe("momentJS", () => {
+//   it("format time", () => {
+    // console.log(moment().format(undefined, "YYYY-MM-DD HH时mm分ss秒"));
+    // console.log(moment().daysInMonth());
+    // console.log(moment().daysInMonth(2020, 2));
+    // console.log(moment().daysInMonth(2021, 2));
+    // console.log(moment().daysInMonth(2020, 12));
+    // console.log(moment().format(undefined, "X"));
+    // console.log(moment().format(undefined, "x"));
+    // console.log(moment().format(null, "YYYY-MM-DD HH时mm分ss秒 LL", true));
+    // console.log(moment().format(undefined, "YYYY-MM-DD HH时mm分ss秒 ll", true));
+    // console.log(moment().format(undefined, "YYYY-MM-DD HH时mm分ss秒", true));
+    // console.log(
+    //   moment().format(undefined, "YYYY-MM-DD HH时mm分ss秒 LL", false)
+    // );
+//   });
+// });
+
+// describe("momentJS", () => {
+//   it("format custom time", () => {
+    // console.log('==>', );
+    // console.log(moment().format(new Date(), "YYYY-MM-DD HH时mm分ss秒"));
+    // console.log(moment().format(1611716340669, "YYYY-MM-DD HH时mm分ss秒 LL", true));
+    // console.log(moment().format('2021-05-01', "YYYY-MM-DD HH时mm分ss秒 LL", true));
+    // console.log(moment().format('2021-05-01 5:38', "YYYY-MM-DD HH时mm分ss秒 ll", true));
+//   });
+// });
+
+
+describe("momentJS", () => {
+  it("get time", () => {
+    // console.log(moment().get("year"));
+    // console.log(moment().get("month"));
+    // console.log(moment().get("date"));
+    // console.log(moment().get("day"));
+    // console.log(moment().get("hours"));
+    // console.log(moment().get("minutes"));
+    // console.log(moment().get("seconds"));
+    // console.log(moment().get("milliseconds"));
+    // console.log(moment().get("localeDay"));
+    // console.log(moment().get());
+    // console.log(moment().toString());
+    // console.log(moment());
+  });
+});
+
+
+describe("momentJS", () => {
+  it("format time", () => {
+    // console.log('==>', );
+    // console.log(moment().format("YYYY-MM-DD HH时mm分ss秒"));
+    // console.log(moment().format(1611716340669, "YYYY-MM-DD HH时mm分ss秒 LL", true));
+    // console.log(moment().format('2021-05-01', "YYYY-MM-DD HH时mm分ss秒 LL", true));
+    // console.log(moment().format('2021-05-01 5:38', "YYYY-MM-DD HH时mm分ss秒 ll", true));
+  });
+});
